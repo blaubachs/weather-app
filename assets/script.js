990627d38f1d1b5d9533c80d28373046c151cc9d
Ben L <veloswim@gmail.com>
2023-01-09 22:26:20 -0800

    added dayjs

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,9 @@
 // current weather
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
+let currentUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid=555fdd6ef60c3be6d13b1f047feabef9";
+
 let forecastUrl =
   "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&units=imperial&appid=555fdd6ef60c3be6d13b1f047feabef9";
 
@@ -20,6 +23,15 @@ fetch(inputCityUrl)
   });
 
 fetch(forecastUrl)
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+fetch(currentUrl)
   .then(function (response) {
     console.log(response);
     return response.json();
